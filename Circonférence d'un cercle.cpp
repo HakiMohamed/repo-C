dfd19e365c677ae387da53b38ec95497d87862ff
Mohamed HAKI <Mohamedhaki70@gmail.com>
2023-09-15 18:35:18 +0200

    Circonférence-du-cercle

---
+++ Circonférence d'un cercle.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main() {
+	const float  Pi = 3.14;
+    float cr , R ;
+    printf("veuiller saisir le rayon du cercle  :");
+    scanf("%f",&R);
+    cr = (R * 2) * Pi;    
+    printf("Circonférence est %.2f",cr);
+    
+	
+	
+	
+	
+	
+	return 0;
+}
+
