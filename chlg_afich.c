4cb6565b4e9f066e903c5ea43983ab8229af77ad
Mohamed HAKI <Mohamedhaki70@gmail.com>
2023-09-15 11:28:25 +0200

    chlg_afich

---
+++ chlg_afich.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+     printf(" NOM ET PRENOM : Haki Mohamed \n");
+     
+      printf(" Age :  21 ans  \n");
+     
+      printf(" Num : +212 6 41725930  \n");
+    
+      printf(" SEX  : HOMME  \n");
+    
+      printf(" NOM : Haki Mohamed \n");
+    return 0;
+}
+
