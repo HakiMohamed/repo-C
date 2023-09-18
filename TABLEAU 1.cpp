35db726c89f00fdc2553cf2686e7d1493a8b7cd8
Mohamed HAKI <Mohamedhaki70@gmail.com>
2023-09-18 21:56:59 +0100

    affich tableau

---
+++ TABLEAU 1.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdio.h>
+
+
+
+int main(){
+	int T[10]{1,2,3,4,5,6,7,8,9,10};
+	int i;
+	
+	for(i=0;i<10;i++){
+		printf("%d",T[i]);
+		if(i<9)
+		printf(",");
+	}
+	
+	
+	
+	
+	return 0;
+}
+
